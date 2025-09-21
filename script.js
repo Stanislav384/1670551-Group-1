7114973beef5df37c6e29bf0ae26059a0bd7d916
NMPmCH <zenabodnaruk739@gmail.com>
2025-09-21 13:16:07 +0100

    добавлений апгрейд(не з повним кодом), оновлений дизайн

--- script.js
+++ script.js
@@ -3,14 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById('footer').innerHTML = `
     <footer>
     </footer>`
-
-
-  if (path.includes("index.html") || path === "/") {
+    
     document.getElementById('header').innerHTML = `
         <header>
             <div class="header-left">
-                <h1 class="header-logo">CHANGESKIN</h1>
-                <a href="#" class="header-button">UPGRADE</a>
+                <a href="index.html"class="header-logo">CHANGESKIN</a>
+                <a href="upgrade.html" class="header-button">UPGRADE</a>
                 <a href="#" class="header-button">CONTRACTS</a>
                 <a href="#" class="header-button">GIVEAWAYS</a>
             </div>
@@ -23,6 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
             </div>
         </header>`
 
+  if (path.includes("index.html") || path === "/") {
     const case_group_list = [ // добавити группу
         {group_id:0, group_name:"Epic cases"},
         {group_id:1, group_name:"Rare cases"}
@@ -30,6 +29,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const case_list = [ //добавити кейс
         {name:"Case", description:"10 Items", price:420, icon:"case_test", group:0},
         {name:"Case", description:"10 Items", price:420, icon:"case_test", group:0},
+        {name:"Case", description:"10 Items", price:420, icon:"case_test", group:0},
+        {name:"Case", description:"10 Items", price:420, icon:"case_test", group:0},
+        {name:"Case", description:"10 Items", price:420, icon:"case_test", group:0},
+        {name:"Case", description:"10 Items", price:420, icon:"case_test", group:0},
+        {name:"Case", description:"10 Items", price:420, icon:"case_test", group:0},
+        {name:"Case", description:"10 Items", price:420, icon:"case_test", group:1},
+        {name:"Case", description:"10 Items", price:420, icon:"case_test", group:1},
+        {name:"Case", description:"10 Items", price:420, icon:"case_test", group:1},
         {name:"Case", description:"10 Items", price:420, icon:"case_test", group:1},
         {name:"Case", description:"10 Items", price:420, icon:"case_test", group:1}
     ]
@@ -63,5 +70,53 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
     load_cases()
-  }
+    }
+    if (path.includes("upgrade.html") || path === "/") {
+        const inventory_items_container = document.getElementById("inventory-items-container")
+        const desired_items_container = document.getElementById("desired-items-container")
+        const inventory_items = [
+            {name:"item",icon:"shadow_item",price:2500},
+            {name:"item",icon:"shadow_item",price:2500},
+            {name:"item",icon:"shadow_item",price:2500},
+            {name:"item",icon:"shadow_item",price:2500},
+            {name:"item",icon:"shadow_item",price:2500}
+        ]
+        const desired_items = [
+            {name:"item",icon:"shadow_item",price:2500},
+            {name:"item",icon:"shadow_item",price:2500},
+            {name:"item",icon:"shadow_item",price:2500},
+            {name:"item",icon:"shadow_item",price:2500},
+            {name:"item",icon:"shadow_item",price:2500}
+        ]
+        function load_items(){
+            inventory_items_container.innerHTML = ""
+            inventory_items.forEach(item => {
+                const div = document.createElement("div")
+                div.className = "item-cell"
+                div.innerHTML = `
+                    <h6 class="item-cell-name">${item.name}</h6>
+                    <img src="icons/${item.icon}.png" class="item-cell-icon">
+                    <div class="item-cell-price">
+                        <h5 class="item-cell-price-text">${item.price}</h5>
+                        <img src="icons/cscoin.png" class="item-cell-price-coin">
+                    </div>`
+                inventory_items_container.appendChild(div)
+            })
+
+            desired_items_container.innerHTML = ""
+            desired_items.forEach(item => {
+                const div = document.createElement("div")
+                div.className = "item-cell"
+                div.innerHTML = `
+                    <h6 class="item-cell-name">${item.name}</h6>
+                    <img src="icons/${item.icon}.png" class="item-cell-icon">
+                    <div class="item-cell-price">
+                        <h5 class="item-cell-price-text">${item.price}</h5>
+                        <img src="icons/cscoin.png" class="item-cell-price-coin">
+                    </div>`
+                desired_items_container.appendChild(div)
+            })
+        }
+        load_items()
+    }
 })
